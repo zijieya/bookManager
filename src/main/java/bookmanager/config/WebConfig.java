dfb86f092432520c8d483d5eabdc4b5133ae4170
zijieya <1347133477@qq.com>
2018-03-20 16:04:25 +0800

    配置thymeleaf

--- src/main/java/bookmanager/config/WebConfig.java
+++ src/main/java/bookmanager/config/WebConfig.java
@@ -9,6 +9,13 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
 //启用springmvc
 //配置DispatcherServlet上下文
 @Configuration
@@ -17,13 +24,35 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 public class WebConfig  extends WebMvcConfigurerAdapter{
     //配置jsp视图解析器
-    @Bean
+  /*  @Bean
     public ViewResolver viewResolver(){
         InternalResourceViewResolver resolver=new InternalResourceViewResolver();
         resolver.setPrefix("/WEB-INF/view/");
         resolver.setSuffix(".jsp");
         resolver.setExposeContextBeansAsAttributes(true);
         return resolver;
+    }*/
+  @Bean
+    public ITemplateResolver templateResolver(){
+         SpringResourceTemplateResolver templateResolver=new SpringResourceTemplateResolver();
+         templateResolver.setPrefix("/WEB-INF/view/");
+         templateResolver.setSuffix(".html");
+         templateResolver.setTemplateMode("HTML5");
+         templateResolver.setCharacterEncoding("UTF-8");
+         return templateResolver;
+    }
+    @Bean
+    public SpringTemplateEngine templateEngine(ITemplateResolver templateResolver){
+      SpringTemplateEngine templateEngine=new SpringTemplateEngine();
+      templateEngine.setTemplateResolver(templateResolver);
+      return  templateEngine;
+    }
+    @Bean
+    public ThymeleafViewResolver viewResolver(SpringTemplateEngine templateEngine){
+        ThymeleafViewResolver viewResolver=new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine);
+        viewResolver.setCharacterEncoding("UTF-8");
+        return viewResolver;
     }
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
